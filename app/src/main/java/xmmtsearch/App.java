84e3f7dada2e00d25e63c2209397f26021c738f9
devendraDPI <devendraindapwar1998@gmail.com>
2024-09-27 11:51:33 +0530

    Remove unnecessary warnings from console

--- app/src/main/java/xmmtsearch/App.java
+++ app/src/main/java/xmmtsearch/App.java
@@ -2,6 +2,9 @@ package xmmtsearch;
 
 public class App {
     public void getGreeting() throws InterruptedException {
+        // This is to remove unnecessary warnings from your console
+        System.setProperty("java.util.logging.config.file", "logging.properties");
+
         TestCases tests = new TestCases(); // Initialize your test class
 
         // TODO: call your test case functions one after other here
